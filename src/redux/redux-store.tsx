6cc90545bbc55f70bfe3b67560ed28b93a866a46
Maxim270693 <maxim.zhuk.93@mail.ru>
2021-06-12 11:45:54 +0300

    devtools extension compose для react и redux

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore,compose} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import {usersReducer} from "./users-reducer";
@@ -16,7 +16,17 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers,applyMiddleware(thunkMiddleware))
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+
+//let store = createStore(reducers,applyMiddleware(thunkMiddleware))
 
 
 export type AllStateType = ReturnType<typeof reducers>
